b367ca1e451036c6862328659d35eb7cb3d0263f
amira <amirabourogaa0@gmail.com>
2021-01-22 13:54:04 +0100

    second commit after deploying the app

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
      <h1>My react application</h1>
      <h2>Author: mirzz loves you all </h2>
      <img  className='image1' src={profileImage} alt='profile image'/>
+     <h>i am happy now that i can deploy free ! </h>
     </div>
   );
 }
